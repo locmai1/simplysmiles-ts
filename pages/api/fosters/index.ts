39681e08d487baac9383993f6a32e0214edadb11
Loc Mai <locmai0315@gmail.com>
2023-06-12 13:51:31 -0400

    add admin only access to GET all fosters route

--- pages/api/fosters/index.ts
+++ pages/api/fosters/index.ts
@@ -17,17 +17,33 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case "GET":
       try {
-        const homes = await prisma.foster.findMany();
+        const { isAdmin } = (await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+          select: {
+            isAdmin: true,
+          },
+        })) ?? { isAdmin: false };
 
-        if (!homes) {
-          res.status(404).json({
-            error: `user ${userId} has no associated foster homes`,
+        if (isAdmin) {
+          const homes = await prisma.foster.findMany();
+
+          if (!homes) {
+            res.status(404).json({
+              error: `user ${userId} has no associated foster homes`,
+            });
+            return;
+          }
+
+          res.status(200).json(homes);
+          return;
+        } else {
+          res.status(500).json({
+            error: "in order to access this route, please sign in as admin",
           });
           return;
         }
-
-        res.status(200).json(homes);
-        return;
       } catch (error) {
         res.status(500).json({
           error: `failed to fetch fosters: ${error}`,
